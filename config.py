419f1aaa334639eb50e05fd5390363791797f1fc
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2023-11-22 22:51:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1001945252825"))
-FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1001616426622"))
+FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "-1002065116797"))
 FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
 FORCE_SUB_5 = int(os.environ.get("FORCE_SUB_5", "0"))
